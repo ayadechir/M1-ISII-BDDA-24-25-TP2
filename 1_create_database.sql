a91e55fcb02975dd21c81375bad56ab899a0130d
aya dechir <133818395+ayadechir@users.noreply.github.com>
2025-04-28 23:50:52 +0100

    Update 1_create_database.sql

--- 1_create_database.sql
+++ 1_create_database.sql
@@ -1,15 +1,22 @@
--- Create LabDatabase Database
+CREATE DATABASE LabDatabase,
 
 GO
 
 -- Use LabDatabase Database
 USE LabDatabase;
 GO 
+CREATE TABLE Laboratoire (
+CodeLab VARCHAR(4) PRIMARY KEY,
+    nomLab VARCHAR(100),
+    Datcreation DATE,
+    DatFin DATE,
+    Siteweb VARCHAR(255));
 
--- Create Laboratoire Table
 
-
--- Create Chercheur Table 
-
+CREATE TABLE Chercheur(
+	NumCh INT PRIMARY KEY,
+    NomCh VARCHAR(30),
+    CodeLab VARCHAR(4),
+    FOREIGN KEY (CodeLab) REFERENCES Laboratoire(CodeLab));
 
 GO
